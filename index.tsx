f8448fa73ccb067db13b36599e8e636e58f488aa
Tu Nombre de Usuario de GitHub <gengepardo@gmail.com>
2025-07-16 09:44:53 -0500

    Final fix: Move CSS to root, add React types, fix TypeScript configuration

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import { NotificationProvider } from './contexts/NotificationContext';
 import { UserProvider } from './contexts/UserContext';
